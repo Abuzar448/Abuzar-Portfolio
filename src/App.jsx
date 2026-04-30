450004a61317fdc32ec5b92b9e716bddf5de3822
Abuzar khan <aburaja2654@gmail.com>
2026-05-01 00:40:51 +0530

    Spacing Issue Correction

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,34 @@ import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import { useEffect, useState } from "react";
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 import About from "./components/About";
 import Certifications from "./components/Certifications";
 import Skills from "./components/Skills";
 import Education from "./components/Education";
 function App() {
-  
   const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
     setIsLoaded(true);
     emailjs.init(import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
   }, []);
-  
+
   return (
-    <div className={`app ${isLoaded ? 'loaded' : ''} relative w-full h-[100vh]`}>
-        <Hero />
-        <About />
-        <Education/>
-        <Skills />
-        <Projects />
-        <Certifications />
-        <Contact />
+    <div
+      className={`app ${isLoaded ? "loaded" : ""} relative w-full h-[100vh]`}
+    >
+      <Hero />
+      <About />
+      <Education />
+      <Skills />
+      <Projects />
+      <Certifications />
+      <Contact />
 
       <>
-      <p className="footer"> @copy: 2025 CodeSeekho.All rights reserved</p>
+        <p className="footer">
+          © {new Date().getFullYear()} | Designed & Developed with Precision by <strong>Abuzar Khan</strong>.
+        </p>
       </>
     </div>
   );
