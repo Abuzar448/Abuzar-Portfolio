3234c26c8da599d701b6ab1b911abc7a787b4f39
Abuzar khan <aburaja2654@gmail.com>
2026-05-04 03:09:24 +0530

    added social links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -168,7 +168,7 @@ const Contact = () => {
               <a href="https://www.linkedin.com/in/abuzar-khan-4a03b0290/"><FaLinkedin /></a>
             </div>
             <div className="w-[50px] h-[50px] border border-blue-300 cursor-pointer flex items-center justify-center bg-[#5c55e2] hover:border-2 hover:border-blue-400 hover:bg-transparent rounded-full">
-              <a href="https://www.instagram.com/abuzar-khan-t/"><FaInstagramSquare /> </a>
+              <a href="https://www.instagram.com/abuzar_khan_t/"><FaInstagramSquare /> </a>
             </div>
             <div className="w-[50px] h-[50px] border border-blue-300 cursor-pointer flex items-center justify-center bg-[#5c55e2] hover:border-2 hover:border-blue-400 hover:bg-transparent rounded-full">
               <a href="https://github.com/Abuzar448"><FaGithub /></a>
