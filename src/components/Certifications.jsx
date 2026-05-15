0f05b7aaf1fefd5adb0497bd8257c9257907ead9
Abuzar khan <aburaja2654@gmail.com>
2026-05-15 20:26:50 +0530

    Added Certificates

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -1,25 +1,22 @@
-import React from 'react'
-import { motion } from 'framer-motion';
+import React from "react";
+import { motion } from "framer-motion";
 
 const Certifications = () => {
-
   const fadeInUp = {
-  initial: { opacity: 0, y: 50 },
-  animate: { opacity: 1, y: 0 },
-  transition: { duration: 0.6 },
-};
+    initial: { opacity: 0, y: 50 },
+    animate: { opacity: 1, y: 0 },
+    transition: { duration: 0.6 },
+  };
 
-const staggerContainer = {
-  animate: {
-    transition: {
-      staggerChildren: 0.5,
+  const staggerContainer = {
+    animate: {
+      transition: {
+        staggerChildren: 0.5,
+      },
     },
-  },
-};
+  };
   return (
-
-     <motion.section
-      id="projects"
+    <motion.section
       className="certification"
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
@@ -31,10 +28,11 @@ const staggerContainer = {
         initial="initial"
         whileInView="animate"
         viewport={{ once: true }}
-        className='font-semibold'
+        className="font-semibold"
       >
         Certifications
       </motion.h2>
+      {/* 1 */}
       <motion.div
         className="certification-grid"
         variants={staggerContainer}
@@ -49,10 +47,42 @@ const staggerContainer = {
         >
           <motion.div
             className="certification-image"
-            style={{ backgroundImage: "url('/projects/FullStack-Certificate.jpg')" }}
+            style={{
+              backgroundImage: "url('/projects/FullStack-Certificate.jpg')",
+            }}
             whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
           />
         </motion.div>
+
+        {/* 2 */}
+        <motion.div
+          className="certification-card"
+          variants={fadeInUp}
+          whileHover={{ y: -10, transition: { duration: 0.2 } }}
+        >
+          <motion.div
+            className="certification-image"
+            style={{
+              backgroundImage: "url('/projects/Internship-Certificate.jpg')",
+            }}
+            whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
+          />
+        </motion.div>
+
+        {/* 3 */}
+        <motion.div
+          className="certification-card"
+          variants={fadeInUp}
+          whileHover={{ y: -10, transition: { duration: 0.2 } }}
+        >
+          <motion.div
+            className="certification-image"
+            style={{ backgroundImage: "url('/projects/Mongodb_Abuzar.jpg')" }}
+            whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
+          />
+        </motion.div>
+
+        {/* 4 */}
 
         <motion.div
           className="certification-card"
@@ -61,17 +91,32 @@ const staggerContainer = {
         >
           <motion.div
             className="certification-image"
-            style={{ backgroundImage: "url('/projects/Internship-Certificate.jpg')" }}
+            style={{
+              backgroundImage: "url('/projects/Cloud_Computing_Abuzar.jpg')",
+            }}
             whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
           />
         </motion.div>
-        
 
-        
-        
+        {/* 5 */}
+
+        <motion.div
+          className="certification-card"
+          variants={fadeInUp}
+          whileHover={{ y: -10, transition: { duration: 0.2 } }}
+        >
+          <motion.div
+            className="certification-image"
+            style={{
+              backgroundImage:
+                "url('/projects/static_visualization_python.jpg')",
+            }}
+            whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
+          />
+        </motion.div>
       </motion.div>
     </motion.section>
-  )
-}
+  );
+};
 
 export default Certifications;
