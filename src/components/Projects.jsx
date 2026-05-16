c00b992078e8e6f2d2e2fed036a7363cd269f15c
Abuzar khan <aburaja2654@gmail.com>
2026-05-16 17:44:33 +0530

    Added new project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -44,91 +44,117 @@ const Projects = () => {
       >
         <a href="https://c1bbe7d5-09c2-4873-8d0c-d9538677d326-00-2in90n41zc1j2.sisko.replit.dev/listings">
           <motion.div
-          className="project-card"
-          variants={fadeInUp}
-          whileHover={{ y: -10, transition: { duration: 0.2 } }}
-        >
-          <motion.div
-            className="project-image"
-            style={{
-              backgroundImage: "url('/projects/Rentmate(portfolio).jpg')",
-            }}
-            whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
-          />
-          <h3> Rentmate - Smart Rental Management System</h3>
-          <p>
-            A full-stack MERN application that streamlines property rental
-            management by connecting tenants and landlords seamlessly.
-          </p>
-          <div className="project-tech">
-            <span>React.js</span>
-            <span>Javascript</span>
-            <span>Node.js</span>
-            <span>Backend API</span>
-            <span>Mongodb</span>
-            <span>Express</span>
-          </div>
-        </motion.div>
+            className="project-card"
+            variants={fadeInUp}
+            whileHover={{ y: -10, transition: { duration: 0.2 } }}
+          >
+            <motion.div
+              className="project-image"
+              style={{
+                backgroundImage: "url('/projects/Rentmate(portfolio).jpg')",
+              }}
+              whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
+            />
+            <h3> Rentmate - Smart Rental Management System</h3>
+            <p>
+              A full-stack MERN application that streamlines property rental
+              management by connecting tenants and landlords seamlessly.
+            </p>
+            <div className="project-tech">
+              <span>React.js</span>
+              <span>Javascript</span>
+              <span>Node.js</span>
+              <span>Backend API</span>
+              <span>Mongodb</span>
+              <span>Express</span>
+            </div>
+          </motion.div>
         </a>
-        
+
         <a href="https://69036271ea3020341e292a15--sidcup-web.netlify.app/">
           <motion.div
-          className="project-card"
-          variants={fadeInUp}
-          whileHover={{ y: -10, transition: { duration: 0.2 } }}
-        >
-          <motion.div
-            className="project-image"
-            style={{
-              backgroundImage: "url('/projects/Frontend(portfolio).jpg')",
-            }}
-            whileHover={{ scale: 1.05 }}
-            transition={{ duration: 0.2 }}
-          />
-          <h3>FlowVerse – The Art of Animated Web Design</h3>
-          <p>
-            A visually engaging website built using HTML, CSS, and JavaScript,
-            enhanced with GSAP animations for smooth, interactive transitions.
-            It focuses on creative motion design and modern UI flow, blending
-            aesthetics with performance.
-          </p>
-          <div className="project-tech">
-            <span>Javascript</span>
-            <span>GSAP</span>
-            <span>CSS</span>
-          </div>
-        </motion.div>
+            className="project-card"
+            variants={fadeInUp}
+            whileHover={{ y: -10, transition: { duration: 0.2 } }}
+          >
+            <motion.div
+              className="project-image"
+              style={{
+                backgroundImage: "url('/projects/Frontend(portfolio).jpg')",
+              }}
+              whileHover={{ scale: 1.05 }}
+              transition={{ duration: 0.2 }}
+            />
+            <h3>FlowVerse – The Art of Animated Web Design</h3>
+            <p>
+              A visually engaging website built using HTML, CSS, and JavaScript,
+              enhanced with GSAP animations for smooth, interactive transitions.
+              It focuses on creative motion design and modern UI flow, blending
+              aesthetics with performance.
+            </p>
+            <div className="project-tech">
+              <span>Javascript</span>
+              <span>GSAP</span>
+              <span>CSS</span>
+            </div>
+          </motion.div>
         </a>
 
         <a href="https://abuzar448.github.io/Myntra_Clone/">
           <motion.div
-          className="project-card"
-          variants={fadeInUp}
-          whileHover={{ y: -10, transition: { duration: 0.2 } }}
-        >
-          <motion.div
-            className="project-image"
-            style={{
-              backgroundImage: "url('/projects/Myntra_Clone.jpg')",
-            }}
-            whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
-          />
-          <h3> StyleHub - Myntra Functional Clone</h3>
-          <p>
-            A fully responsive, functional web application that replicates the core user interface and "Add to Bag" functionality of the popular e-commerce platform, Myntra.
-          </p>
-          <div className="project-tech">
-            <span>HTML5</span>
-            <span>Javascript</span>
-            <span>CSS5</span>
-            <span>Tailwind</span>
-            <span>Media Queries</span>
-          </div>
-        </motion.div>
+            className="project-card"
+            variants={fadeInUp}
+            whileHover={{ y: -10, transition: { duration: 0.2 } }}
+          >
+            <motion.div
+              className="project-image"
+              style={{
+                backgroundImage: "url('/projects/Myntra_Clone.jpg')",
+              }}
+              whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
+            />
+            <h3> StyleHub - Myntra Functional Clone</h3>
+            <p>
+              A fully responsive, functional web application that replicates the
+              core user interface and "Add to Bag" functionality of the popular
+              e-commerce platform, Myntra.
+            </p>
+            <div className="project-tech">
+              <span>HTML5</span>
+              <span>Javascript</span>
+              <span>CSS5</span>
+              <span>Tailwind</span>
+              <span>Media Queries</span>
+            </div>
+          </motion.div>
         </a>
 
-        
-        
+        <a href="https://ems-react-nu.vercel.app/">
+          <motion.div
+            className="project-card"
+            variants={fadeInUp}
+            whileHover={{ y: -10, transition: { duration: 0.2 } }}
+          >
+            <motion.div
+              className="project-image"
+              style={{
+                backgroundImage: "url('/projects/EMS.jpg')",
+              }}
+              whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
+            />
+            <h3>SyncroWork: Enterprise Task Monitoring System</h3>
+            <p>
+              A React & Tailwind-based system featuring a dual-interface for Admin task assignment and Employee progress tracking. Fully responsive, high-performance, and powered by Context API for seamless, real-time state management.
+            </p>
+            <div className="project-tech">
+              <span>React.js</span>
+              <span>Javascript</span>
+              <span>CSS5</span>
+              <span>Tailwind</span>
+              <span>Media Queries</span>
+            </div>
+          </motion.div>
+        </a>
       </motion.div>
     </motion.section>
   );
